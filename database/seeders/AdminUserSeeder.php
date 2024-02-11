a669cb4519aa582e4e13f72610a9569f23b2269a
Hosnol Arifin <hosnolarifin220@gmail.com>
2024-02-11 07:39:48 +0700

    chore: Memuat seeder untuk menambah pengguna di tabel users

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Tambahkan user admin baru
+        DB::table('users')->insert([
+            'id_karyawan' => 'ADM001',
+            'nama' => 'Andika Fahrurozi',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+    }
+}
